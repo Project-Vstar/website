ef6c385b1fe7311cf1d89278583e4bd9ddc59dc0
aymenae <aymenebrahim15@gmail.com>
2025-08-18 03:17:42 +0200

    Feat: Added vstar & vinfernia pages

---
+++ src/app/vstar/page.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function VStarPage() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-foreground text-white text-4xl">
+        VSTAR Page
+    </div>
+  );
+}
