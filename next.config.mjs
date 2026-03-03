488353b53160fff680067390e25b7db67681c318
Aymen Ebrahim <aymenebrahim15@gmail.com>
2026-03-03 15:45:49 +0100

    Fix: Added json to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+    experimental: {
+    turbo: {
+      resolveExtensions: ['.js', '.jsx', '.json'],
+    },
+  },
 };
 
 export default nextConfig;
